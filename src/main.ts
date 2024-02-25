6d6bb760e98cabf712f06d70d7c322573fc93d55
David Mensah <davidmensahedem@gmail.com>
2024-02-25 12:54:52 -0800

    add port to the env file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AtGuard } from './common/guards';
+import { env } from 'process';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors:true});
   app.useGlobalPipes(new ValidationPipe);
-  await app.listen(3619);
+  await app.listen(env.APP_PORT);
 }
+
 bootstrap();
