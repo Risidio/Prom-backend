d9bd218d7c7bac62663f1578d4df87013f521638
David Mensah <davidmensahedem@gmail.com>
2024-03-04 13:44:12 +0000

    trigger deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AtGuard } from './common/guards';
 import { env } from 'process';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
